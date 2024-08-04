18527a494862966a777a406dd82cad464406f5d0
Zaid Syed <147842018+ZaidmSyed@users.noreply.github.com>
2024-08-04 12:59:10 -0700

    Initial Commit

---
+++ UserManagementQuickStart/UserManagementQuickStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserManagementQuickStartApp.swift
+//  UserManagementQuickStart
+//
+//  Created by Zaid Syed on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct UserManagementQuickStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
